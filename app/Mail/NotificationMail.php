db5a7ac24b1f0d6cb39509454eae78ac21342b18
Ben Muchiri <bernard.njoroge@strathmore.edu>
2023-12-17 19:12:17 +0300

    Mail notif

--- app/Mail/NotificationMail.php
+++ app/Mail/NotificationMail.php
@@ -39,7 +39,7 @@ class NotificationMail extends Mailable
     public function content(): Content
     {
         return new Content(
-            view: 'view.name',
+            view: 'mail.notification_mail',
         );
     }
 
