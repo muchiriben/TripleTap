3c66b8591624ec3997a08d48d1d6a5d476119ebe
Ben Muchiri <bernard.njoroge@strathmore.edu>
2023-12-27 14:49:27 +0300

    Mobile guest form

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false])
 
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-300 bg-light-color text-secondary-color focus:border-primary-color focus:ring-indigo-500 rounded-md shadow-sm']) !!}>
+<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-secondary-color bg-light-color text-secondary-color focus:border-primary-color focus:ring-indigo-500 rounded-md shadow-sm']) !!}>
