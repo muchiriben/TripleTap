a2c4ab8d9206ebc99f4855bdd150d724edbaf02f
Ben Muchiri <bernard.njoroge@strathmore.edu>
2024-02-01 12:37:07 +0300

    Add to cart

--- resources/views/guest/shop/products.blade.php
+++ resources/views/guest/shop/products.blade.php
@@ -75,6 +75,7 @@
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 lg:h-8 lg:w-8" fill="none" viewBox="0 0 24 24" stroke="#F2F7FF" stroke-width="2">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M12 4v16m8-8H4" /> 
                           </x-primary-button>
+                          <span class="font-medium text-sm text-center">Add To Cart</span>
                           </form> 
                         
                      </div>
